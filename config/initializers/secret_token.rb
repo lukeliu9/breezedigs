1f557a102b810601a337d8383281590386a0793f
Luke <luketliu@gmail.com>
2013-04-17 22:03:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breezedigs::Application.config.secret_token = '02d99caa0d2502db220d410d403e890deaa1f2f5ca681bc9cbd5aef2382692b927de2b188d852943d1a3e200c8a49ef731f1143f78f33d0beb594dd903ab0573'
