1d6cfaed454bb4fca6903802a60cc0b59c2ebbe1
Luke <luketliu@gmail.com>
2013-06-07 19:04:47 -0500

    fixed paperclip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :image, :building_id
 
+  has_attached_file :image, :default_url => "/images/:style/nobuildingimage.png", :path => "app/public/system/images/:id/:style/:basename.:extension"
+
   belongs_to :user
   belongs_to :building
 
