1d4c6abc0aefc13623785d6f8e6f7dc397f159f4
Luke <luketliu@gmail.com>
2013-06-07 20:52:35 -0500

    paperclip take 4

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,15 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :image, :building_id
 
-  has_attached_file :image, :default_url => "/images/:style/nobuildingimage.png"
+  has_attached_file :image, 
+  :default_url => "/images/:style/nobuildingimage.png",
+  :storage => :s3,
+  :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  },
+  :path => "/images/:attachment/:id.:extension"
 
   belongs_to :user
   belongs_to :building
