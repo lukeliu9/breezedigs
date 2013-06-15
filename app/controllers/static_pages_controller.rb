7a3494de486a426bb4be6538bbd49aa58d509634
Luke <luketliu@gmail.com>
2013-06-15 13:56:51 -0500

    links on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @buildings = Building.all.count
-    @best = Building.has_reviews.get_best_buildings.first(6)
+    @best = Building.get_best_buildings.first(6)
     @worst = Building.has_reviews.get_worst_buildings.first(6)
     @featured = Building.select_only_in_city("Chicago").has_image.sort_featured("count").first(6)
     @recent_reviews = Review.get_recent_reviews(5)
